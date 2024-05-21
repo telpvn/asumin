342b90d4b6aa3ac2226bc1dc1d9cad039f2537e3
Professional <telpvn@mail.ru>
2024-05-21 16:23:04 +0400

    Double volume

--- asumin/oop/oop/src/Introductiontomethods/Box.java
+++ asumin/oop/oop/src/Introductiontomethods/Box.java
@@ -5,8 +5,8 @@ public class Box {
     double height;
     double length;
 
-    void showVolume() {
-        double volume = width * height * length;
-        System.out.println(volume);
+    double volume() {
+        return width * height * length;
     }
+
 }
--- asumin/oop/oop/src/Introductiontomethods/Main.java
+++ asumin/oop/oop/src/Introductiontomethods/Main.java
@@ -15,7 +15,10 @@ public class Main {
         box2.height = 10;
         box2.width = 10;
 
-        box1.showVolume();
-        box2.showVolume();
+        double volume1 = box1.volume();
+        double volume2 = box2.volume();
+
+        System.out.println(volume1);
+        System.out.println(volume2);
     }
 }
