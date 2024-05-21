29755d9889c676e1d319227c92dd22f201e0e74b
Professional <telpvn@mail.ru>
2024-05-21 16:11:39 +0400

    Void showVolume

--- asumin/oop/oop/src/Introductiontomethods/Box.java
+++ asumin/oop/oop/src/Introductiontomethods/Box.java
@@ -4,4 +4,9 @@ public class Box {
     double width;
     double height;
     double length;
+
+    void showVolume() {
+        double volume = width * height * length;
+        System.out.println(volume);
+    }
 }
--- asumin/oop/oop/src/Introductiontomethods/Main.java
+++ asumin/oop/oop/src/Introductiontomethods/Main.java
@@ -2,6 +2,20 @@ package Introductiontomethods;
 
 public class Main {
     public static void main(String[] args) {
+        Box box1 = new Box();
 
+        box1.length = 5;
+        box1.height = 5;
+        box1.width = 5;
+
+
+        Box box2 = new Box();
+
+        box2.length = 10;
+        box2.height = 10;
+        box2.width = 10;
+
+        box1.showVolume();
+        box2.showVolume();
     }
 }
