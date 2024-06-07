26aa808a8ee2aa622d3e18bb33a67957d3267ec2
Professional <telpvn@mail.ru>
2024-06-07 10:04:51 +0400

    ArrayList Methods and Related Methods 2

--- ztregulov/сollections/Collections/src/сollections/ArrayListMethods5.java
+++ ztregulov/сollections/Collections/src/сollections/ArrayListMethods5.java
@@ -1,6 +1,7 @@
 package сollections;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListMethods5 {
     public static void main(String[] args) {
@@ -9,15 +10,46 @@ public class ArrayListMethods5 {
         arrayList1.add("Ivan");
         arrayList1.add("Mariya");
         arrayList1.add("Kolya");
-        System.out.println(arrayList1);
+        arrayList1.add("Elena");
 
-        ArrayList<String> arrayList2 = new ArrayList<>();
-        arrayList2.add("Ivan");
-        arrayList2.add("Mariya");
-        arrayList2.add("Igor");
+        List<Integer> list1 = List.of(3, 8, 13);
+        System.out.println(list1);
+//        list1.add(100);
+
+        List<String> list2 = List.copyOf(arrayList1); // изменять, null нельзя
+        System.out.println(list2);
+
+//        System.out.println("ArrayList = " + arrayList1);
+//
+//        Object[] array = arrayList1.toArray();
+//
+//        String[] array2 = arrayList1.toArray(new String[0]);
+//
+//        for (String s : array2) {
+//            System.out.println(s);
+//        }
+
+//        List<String> myList = arrayList1.subList(1, 4);
+//        System.out.println("SubList = " + myList);
+//        myList.add("Fedor");
+//        System.out.println("SubList = " + myList);
+//        System.out.println("ArrayList = " +arrayList1);
+//        arrayList1.add("Sveta");
+//        System.out.println("ArrayList = " +arrayList1);
+//        System.out.println("SubList = " + myList);
+
+
+//        ArrayList<String> arrayList2 = new ArrayList<>();
+//        arrayList2.add("Ivan");
+//        arrayList2.add("Mariya");
+//        arrayList2.add("Igor");
 
 //        arrayList1.removeAll(arrayList2);
-        arrayList1.retainAll(  arrayList2);
-        System.out.println(arrayList1);
+//        arrayList1.retainAll(  arrayList2);
+//        System.out.println(arrayList1);
+
+//        boolean result = arrayList1.containsAll(arrayList2);
+//        System.out.println(result);
+
     }
 }
